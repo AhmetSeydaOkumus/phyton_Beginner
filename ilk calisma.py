3d7ae4494bcebaf345470fc23774641c6bf9c85d
Ahmet Okumus <ahmetseydaokumus.business@gmail.com>
2021-11-27 17:24:12 +0100

    first_Loading

---
+++ ilk calisma.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[28]:
+
+
+x=800 
+
+
+# In[29]:
+
+
+y="merhaba"
+
+
+# In[30]:
+
+
+print(x,y, sep="yyyy")
+
+
+# In[43]:
+
+
+print("merhaba"+y)
+
+
+# In[39]:
+
+
+name=input("isminiz nedir?")
+
+
+# In[41]:
+
+
+print("merhaba"+name)
+
+
+# In[46]:
+
+
+if 5>2:
+    print("hello")
+
+
+# In[47]:
+
+
+#dsxxcv
+"""sadasdasdasd
+jhjh"""
+print("x")
+
+
+# In[51]:
+
+
+fruit=["banana","strawberry","lemon"]
+x,y,z=fruit
+print(x)
+print("y")
+print("z")
+
+
+# In[52]:
+
+
+n=300
+
+
+# In[53]:
+
+
+m=n
+
+
+# In[54]:
+
+
+print(m)
+
+
+# In[55]:
+
+
+x="abc"
+
+
+# In[56]:
+
+
+y=x
+
+
+# In[58]:
+
+
+print(y)
+
+
+# In[59]:
+
+
+id(x)
+
+
+# In[60]:
+
+
+id(y)
+
+
+# In[61]:
+
+
+id(n)
+
+
+# In[62]:
+
+
+id(m)
+
+
+# In[63]:
+
+
+x="abz"
+
+
+# In[64]:
+
+
+print(y)
+
+
+# In[65]:
+
+
+n="400"
+
+
+# In[66]:
+
+
+print(m)
+
+
+# In[76]:
+
+
+isim="ahmet"
+soyisim="marti"
+sehir="malatya"
+
+
+# In[77]:
+
+
+print("isim :", isim, "\n", "soyisim :" ,soyisim,"\n", "sehir :",sehir)
+
+
+# In[79]:
+
+
+x,y,z="ahmet","ayse","mahmut"
+
+
+# In[80]:
+
+
+print(x)
+
+
+# In[81]:
+
+
+y
+
+
+# In[84]:
+
+
+x=50
+if x<20:
+    print("smaller")
+if x>40:
+    print("bigger")
+print("Finish")
+
+
+# In[96]:
+
+
+r=12
+
+
+# In[100]:
+
+
+while r>10:
+    print(r)
+    r=r-1
+print ("murekkep bitti")
+
+
+# In[118]:
+
+
+name=int(input("yasinis kac?"))
+name1=int(input("annenizin yasi kac?"))
+print("anne ve cocugun toplam yasi", name+name1)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
